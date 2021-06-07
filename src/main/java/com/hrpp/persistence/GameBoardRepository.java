aa7d760c0da17214bbc4e2c226c9bf42440109c8
KHR <rimiha9708@gmail.com>
2021-06-07 12:34:20 +0900

    GameBoardRepository +테스트

--- src/main/java/com/hrpp/persistence/GameBoardRepository.java
+++ src/main/java/com/hrpp/persistence/GameBoardRepository.java
@@ -1,5 +1,9 @@
 package com.hrpp.persistence;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.hrpp.domain.GameBoard;
@@ -7,4 +11,42 @@ import com.hrpp.domain.GameBoard;
 public interface GameBoardRepository 
 	extends CrudRepository<GameBoard, Long>{
 
+	//findBy~
+	//title 조건으로 검색
+	//where title = ?
+	public List<GameBoard> findByTitle(String title);
+	//writer 조건으로 검색
+	//where writer=?
+	public Collection<GameBoard> findByWriter(String writer);
+	
+	public List<GameBoard> findByContent(String content);
+	
+	//writer의 like 구문 처리
+	public List<GameBoard> findByWriterContaining(String writer);
+	
+	//title에 '7'이 들어가 있는 레코드 검색
+	public List<GameBoard> findByTitleContaining(String title);
+	
+	//content에 '9채우기'로 끝나는 레코드 검색
+	public List<GameBoard> findByContentEndingWith(String content);
+	
+	// OR조건의 처리 ? title like '%title%' or content like '%content%'
+	public List<GameBoard> findByTitleContainingOrContentContaining
+	(String title, String content);
+	
+	//title like '%title%' and bno >
+	// bno값이 100보다는 크고 title에는 5가 들어가게 검색
+	public List<GameBoard> findByTitleContainingAndBnoGreaterThan
+	(String title,Long bno);
+	
+	//bno > ? order by bno desc
+	// bno값이 90보다 크게
+	public List<GameBoard> findByBnoGreaterThanOrderByBnoDesc(Long bno);
+	
+	//bno > ? order by bno desc limit ? , ?
+	//import org.springframework.data.domain.Pageable;
+	//페이징 처리를 위해 10개씩 보이게 처리
+	public List<GameBoard> findByBnoGreaterThanOrderByBnoDesc
+	(Long bno, Pageable paging);
+	
 }
--- src/test/java/com/hrpp/GameBoardApplicationTests.java
+++ src/test/java/com/hrpp/GameBoardApplicationTests.java
@@ -1,13 +1,103 @@
 package com.hrpp;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import com.hrpp.domain.GameBoard;
+import com.hrpp.persistence.GameBoardRepository;
 
 @SpringBootTest
 class GameBoardApplicationTests {
 
+	@Autowired
+	private GameBoardRepository repo;
+	
 	@Test
-	void contextLoads() {
+	void contextCreate() {
+		System.out.println("테이블 생성 확인");
+	}
+	
+	@Test
+	public void testInsert200() {
+		for (int i=0; i<200; i++) {
+			GameBoard board = new GameBoard();
+			board.setTitle("제목..."+i);
+			board.setContent("내용..."+i+"채우기");
+			board.setWriter("user0"+(i%10));
+			repo.save(board);
+		}
+	}
+	
+	@Test
+	public void testByWriter() {
+		Collection<GameBoard> result = repo.findByWriter("user00");
+		result.forEach(GameBoard -> System.out.println(GameBoard));
+		
+		repo.findByWriter("user00").forEach(GameBoard -> System.out.println(GameBoard));
+	}
+	
+	@Test
+	public void testByContent() {
+		List<GameBoard> results = repo.findByContent("내용...0채우기");
+		results.forEach(GameBoard -> System.out.println(GameBoard));
+	}
+	
+	@Test
+	public void testByWriterContaining() {
+		List<GameBoard> results = repo.findByWriterContaining("05");
+		results.forEach(GameBoard -> System.out.println(GameBoard));
 	}
 
+	@Test
+	public void testByTitleContaining() {
+		List<GameBoard> results = repo.findByTitleContaining("7");
+		results.forEach(GameBoard -> System.out.println(GameBoard));
+	}
+	
+	@Test
+	public void testByContentEndingWith() {
+		List<GameBoard> results = repo.findByContentEndingWith("9채우기");
+		results.forEach(GameBoard -> System.out.println(GameBoard));
+	}
+	
+	@Test
+	public void testByTitleOrContent() {
+		List<GameBoard> results = repo.findByTitleContainingOrContentContaining("77", "55");
+		results.forEach(GameBoard -> System.out.println(GameBoard));
+	}
+	
+	@Test
+	public void testByTitleAndBno() {
+		List<GameBoard> results  = 
+				repo.findByTitleContainingAndBnoGreaterThan("5", 100L);
+		results.forEach(GameBoard -> System.out.println(GameBoard));
+	}
+	
+	@Test
+	public void testByBnoOrderByBno() {
+		List<GameBoard> results  = 
+				repo.findByBnoGreaterThanOrderByBnoDesc(90L);
+		results.forEach(GameBoard -> System.out.println(GameBoard));
+	}
+	
+	@Test
+	public void testBnoOrderByPaging() {
+		Pageable paing = PageRequest.of(0, 10);
+		List<GameBoard> results  = 
+				repo.findByBnoGreaterThanOrderByBnoDesc(0L, paing);
+		results.forEach(board -> System.out.println(board));
+	}
+
+
 }
+
+
+
+
+
